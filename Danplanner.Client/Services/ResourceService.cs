153c058e1dc83776bc682cfa61d03611925f96f4
FairyLlama <FairyLlama@users.noreply.github.com>
2025-11-19 23:07:46 +0100

    forgot 1 file

--- Danplanner.Client/Services/ResourceService.cs
+++ Danplanner.Client/Services/ResourceService.cs
@@ -1,18 +1,18 @@
-﻿using Danplanner.Shared.Models;
-using System.Net.Http.Json;
+﻿// using Danplanner.Shared.Models;
+// using System.Net.Http.Json;
 
-namespace Danplanner.Client.Services
-{
-    public class ResourceService : IResourceService
-    {
-        private readonly HttpClient _http;
-        public ResourceService(IHttpClientFactory factory) => _http = factory.CreateClient("EF");
+// namespace Danplanner.Client.Services
+// {
+//     public class ResourceService : IResourceService
+//     {
+//         private readonly HttpClient _http;
+//         public ResourceService(IHttpClientFactory factory) => _http = factory.CreateClient("EF");
 
-        public async Task<List<ResourceDto>> GetAllAsync() =>
-            await _http.GetFromJsonAsync<List<ResourceDto>>("api/resource") ?? new();
+//         public async Task<List<ResourceDto>> GetAllAsync() =>
+//             await _http.GetFromJsonAsync<List<ResourceDto>>("api/resource") ?? new();
 
-        public async Task<ResourceDto?> GetByIdAsync(int id) =>
-            await _http.GetFromJsonAsync<ResourceDto>($"api/resource/{id}");
-    }
+//         public async Task<ResourceDto?> GetByIdAsync(int id) =>
+//             await _http.GetFromJsonAsync<ResourceDto>($"api/resource/{id}");
+//     }
 
-}
+// }
