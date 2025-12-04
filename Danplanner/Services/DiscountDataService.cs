5da8a4542d1c031feab029e1d10219f7bc7e032c
FairyLlama <FairyLlama@users.noreply.github.com>
2025-12-04 14:44:20 +0100

    WIP

--- Danplanner/Services/DiscountDataService.cs
+++ Danplanner/Services/DiscountDataService.cs
@@ -3,7 +3,7 @@
 // using Danplanner.Shared.Models;
 // using Microsoft.EntityFrameworkCore;
 // namespace Danplanner.Services;
-// public class DiscountDataService (AppDbContext db) : IDiscountDataService
+// public class DiscountDataService (AppDbContext db)
 // {
-    
+//     private readonly AppDbContext _db = db;
 // }
