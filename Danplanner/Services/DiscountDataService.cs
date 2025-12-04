8bfd08da0cbf7cfcaa1fb8d5f5c00d984f1abb8e
FairyLlama <FairyLlama@users.noreply.github.com>
2025-12-04 09:22:26 +0100

    fixed file

---
+++ Danplanner/Services/DiscountDataService.cs
@@ -0,0 +1,9 @@
+// using Danplanner.Data;
+// using Danplanner.Data.Entities;
+// using Danplanner.Shared.Models;
+// using Microsoft.EntityFrameworkCore;
+// namespace Danplanner.Services;
+// public class DiscountDataService (AppDbContext db) : IDiscountDataService
+// {
+    
+// }
